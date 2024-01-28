a7485144910a4cb32d76f74309a14909aacd0bd8
jakub <jakub.kopco@hardsun.eu>
2024-01-28 19:11:10 +0100

    remove fire auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import {getFirestore, provideFirestore} from "@angular/fire/firestore";
 import {RouterOutlet} from "@angular/router";
-import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 
 @NgModule({
   declarations: [],
   imports: [
-    AngularFireAuthModule,
-    provideFirestore(() => getFirestore()),
     MatSlideToggleModule,
     BrowserModule,
     BrowserAnimationsModule,
-    RouterOutlet,
+    RouterOutlet
   ],
   providers: [],
   bootstrap: []
